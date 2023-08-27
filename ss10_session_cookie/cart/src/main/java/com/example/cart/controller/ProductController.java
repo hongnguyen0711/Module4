2e1f003ba1e004e6b8c17ba88bf246f722a62884
hongnguyen0711 <nguyenhongnguyen40@gmail.com>
2023-08-28 00:21:08 +0700

    ss10

--- ss10_session_cookie/cart/src/main/java/com/example/cart/controller/ProductController.java
+++ ss10_session_cookie/cart/src/main/java/com/example/cart/controller/ProductController.java
@@ -1,30 +1,46 @@
 package com.example.cart.controller;
 
 import com.example.cart.model.Cart;
+import com.example.cart.model.Product;
 import com.example.cart.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.xml.ws.Action;
+import java.util.Optional;
 
 @Controller
 @SessionAttributes("cart")
 public class ProductController {
     @Autowired
     private IProductService service;
+
     @ModelAttribute("cart")
-    public Cart setupCart(){
+    public Cart setupCart() {
         return new Cart();
     }
+
     @GetMapping("/shop")
-    public ModelAndView showShop(){
+    public ModelAndView showShop() {
         ModelAndView modelAndView = new ModelAndView("/shop");
         modelAndView.addObject("products", service.findAll());
         return modelAndView;
+    }
+
+    @GetMapping("/add/{id}")
+    public String addToCart(@PathVariable Long id, @ModelAttribute Cart cart, @RequestParam("action") String action) {
+        Optional<Product> productOptional = service.findById(id);
+        if (!productOptional.isPresent()) {
+            return "/error_404";
+        }
+        if (action.equals("show")) {
+            cart.addProduct(productOptional.get());
+            return "redirect:/shopping-cart";
+        }
+        cart.addProduct(productOptional.get());
+        return "redirect:/shop";
     }
 }
